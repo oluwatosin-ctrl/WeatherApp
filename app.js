2dc497a7eb9374390c5a7c84bab61990233a8e5c
Mofikoya Oluwatosin <58105807+oluwatosin-ctrl@users.noreply.github.com>
2020-09-10 15:51:06 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+var button = document.querySelector('.button')
+var inputvalue = document.querySelector('.inputvalue')
+var nane = document.querySelector('.nane');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+button.addEventListener('click',function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid=55b52394dec57189292c5378a4e35bbd')
+    .then(response => response.json())
+    .then(data => {
+        var namevalue = data['name'];
+        var tempvalue = data['main']['temp'];
+        var descvalue = data['weather'][0]['description'];
+
+        inputvalue.innerHTML = inputvalue;
+        nane.innerHTML = namevalue;
+        temp.innerHTML = tempvalue;
+        desc.innerHTML = descvalue;
+    })
+
+.catch(err => alert("Wrong city name!"))
+})
+
+/**var namevalue = data['name'];
+        var tempvalue = data['main']['temp'];
+        var descvalue = data['weather'][0]['description'];
+
+        name.innerHTML = namevalue;
+        temp.innerHTML = tempvalue;
+        desc.innerHTML = descvalue;
+ */
